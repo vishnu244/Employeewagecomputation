1d4505e2025ed42ff18f21e29651a967a1423af6
vishnu244 <vishnunali244@gmail.com>
2022-04-21 19:09:45 +0530

    Calculating wage for ax hours and days

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -5,36 +5,58 @@ Console.WriteLine("Welcome to Employee Wage computation program!");
 
 int empwageperhrs = 20;
 int emphrs = 0;
-int workingdays = 20;
+int workingdays = 0;
+
+int maxworkinghrs = 100;
+int maxworkingdays = 20;
+
 Random CheckempAttandance = new Random();
 int ispresent = CheckempAttandance.Next(0, 4);
 Console.WriteLine(ispresent);
 
-
-
-switch (ispresent)
+if (ispresent == 1)
 {
-    case 1:
-        Console.WriteLine("Employee is present!");
-        Console.WriteLine("Employee is Full Time");
-        emphrs = 8;
-        break;
-    case 2:
-        Console.WriteLine("Employee is present!");
-        Console.WriteLine("Employee is Part Time");
-        emphrs = 4;
-        break;
-    case 0:
-        Console.WriteLine("Employee is Absent!");
-
-        emphrs = 0;
-        break;
-    default:
-        Console.WriteLine("error");
-        break;
+    Console.WriteLine("Employee is present!");
+    Console.WriteLine("Employee is Full Time");
 
 }
-int Salary = emphrs * empwageperhrs;
-int monthsalary = Salary * workingdays;
-Console.WriteLine("Employee Salary = " + Salary);
-Console.WriteLine("Finalmonthsalary = " + monthsalary);
+else if (ispresent == 2)
+{
+    Console.WriteLine("Employee is Present!");
+    Console.WriteLine("Employee is Part Time");
+
+}
+
+else
+{
+    Console.WriteLine("Employee is Absent!");
+
+}
+
+while ((emphrs < maxworkinghrs) && (workingdays < maxworkingdays))
+{
+    switch (ispresent)
+    {
+        case 1:
+            emphrs += 8;
+            break;
+        case 2:
+            emphrs += 4;
+            break;
+        case 0:
+
+            emphrs = 0;
+            break;
+        default:
+            Console.WriteLine("error");
+            break;
+    }
+    workingdays++;
+
+}
+
+
+Console.WriteLine("Total Working Hours : " + emphrs);
+Console.WriteLine("Total working Days Per Month : " + workingdays);
+int monthsalary = empwageperhrs * emphrs;
+Console.WriteLine("Total Salary of Employee per Month : " + monthsalary);
