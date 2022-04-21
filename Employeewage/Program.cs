3741e784353ab35f48743d3613bff89ae434395e
vishnu244 <vishnunali244@gmail.com>
2022-04-21 19:00:24 +0530

    Adding Part time employee

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -6,13 +6,20 @@ Console.WriteLine("Welcome to Employee Wage computation program!");
 int empwageperhrs = 20;
 int emphrs;
 Random CheckempAttandance = new Random();
-int ispresent = CheckempAttandance.Next(0, 2);
+int ispresent = CheckempAttandance.Next(0, 3);
 Console.WriteLine(ispresent);
 if (ispresent == 1)
 {
     Console.WriteLine("Employee is present!");
+    Console.WriteLine("Employee is Full Time");
     emphrs = 8;
 
+}
+else if (ispresent == 2)
+{
+    Console.WriteLine("Employee is Present!");
+    Console.WriteLine("Employee is Part Time");
+    emphrs = 4;
 }
 
 else
