216849baea943ea6a268c300260fcb94a43efc66
vishnu244 <vishnunali244@gmail.com>
2022-04-21 19:02:40 +0530

    using switch case

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -4,28 +4,30 @@
 Console.WriteLine("Welcome to Employee Wage computation program!");
 
 int empwageperhrs = 20;
-int emphrs;
+int emphrs = 0;
 Random CheckempAttandance = new Random();
 int ispresent = CheckempAttandance.Next(0, 3);
 Console.WriteLine(ispresent);
-if (ispresent == 1)
-{
-    Console.WriteLine("Employee is present!");
-    Console.WriteLine("Employee is Full Time");
-    emphrs = 8;
 
-}
-else if (ispresent == 2)
-{
-    Console.WriteLine("Employee is Present!");
-    Console.WriteLine("Employee is Part Time");
-    emphrs = 4;
-}
 
-else
+
+switch (ispresent)
 {
-    Console.WriteLine("Employee is Absent!");
-    emphrs = 0;
+    case 1:
+        Console.WriteLine("Employee is present!");
+        Console.WriteLine("Employee is Full Time");
+        emphrs = 8;
+        break;
+    case 2:
+        Console.WriteLine("Employee is present!");
+        Console.WriteLine("Employee is Full Time");
+        emphrs = 4;
+        break;
+    case 0:
+        Console.WriteLine("Employee is present!");
+        Console.WriteLine("Employee is Full Time");
+        emphrs = 0;
+        break;
 
 }
 int Salary = emphrs * empwageperhrs;
