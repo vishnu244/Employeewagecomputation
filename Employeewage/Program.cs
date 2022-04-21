854f748d61508fcfe0854e2ff609ce1fe5fc7ed0
vishnu244 <vishnunali244@gmail.com>
2022-04-21 19:05:11 +0530

    calculating monthly wage

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -5,8 +5,9 @@ Console.WriteLine("Welcome to Employee Wage computation program!");
 
 int empwageperhrs = 20;
 int emphrs = 0;
+int workingdays = 20;
 Random CheckempAttandance = new Random();
-int ispresent = CheckempAttandance.Next(0, 3);
+int ispresent = CheckempAttandance.Next(0, 4);
 Console.WriteLine(ispresent);
 
 
@@ -20,15 +21,20 @@ switch (ispresent)
         break;
     case 2:
         Console.WriteLine("Employee is present!");
-        Console.WriteLine("Employee is Full Time");
+        Console.WriteLine("Employee is Part Time");
         emphrs = 4;
         break;
     case 0:
-        Console.WriteLine("Employee is present!");
-        Console.WriteLine("Employee is Full Time");
+        Console.WriteLine("Employee is Absent!");
+
         emphrs = 0;
+        break;
+    default:
+        Console.WriteLine("error");
         break;
 
 }
 int Salary = emphrs * empwageperhrs;
+int monthsalary = Salary * workingdays;
 Console.WriteLine("Employee Salary = " + Salary);
+Console.WriteLine("Finalmonthsalary = " + monthsalary);
