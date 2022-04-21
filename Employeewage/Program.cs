c6da2beac757977d1461dc1acd8c6c3e96d6a338
vishnu244 <vishnunali244@gmail.com>
2022-04-21 18:57:45 +0530

    Employee present or absent

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -1,12 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Employeewage computation!");
+
 
 Console.WriteLine("Welcome to Employee Wage computation program!");
 
-Random Attendancecheck = new Random();
-int ispresent = Attendancecheck.Next(0, 2);
+int empwageperhrs = 20;
+int emphrs;
+Random CheckempAttandance = new Random();
+int ispresent = CheckempAttandance.Next(0, 2);
 Console.WriteLine(ispresent);
 if (ispresent == 1)
+{
     Console.WriteLine("Employee is present!");
+    emphrs = 8;
+
+}
+
 else
-    Console.WriteLine("Employee is Absent");
+{
+    Console.WriteLine("Employee is Absent!");
+    emphrs = 0;
+
+}
+int Salary = emphrs * empwageperhrs;
+Console.WriteLine("Employee Salary = " + Salary);
