146fa893174b7ef150872eecca26c19deb85aaa6
vishnu244 <vishnunali244@gmail.com>
2022-04-29 08:28:22 +0530

    [Vishnu] Total employee wage per each company

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -4,10 +4,16 @@
 using Employeewage;
 
 Console.WriteLine("Welcome to Employee Wage computation program!");
+Console.WriteLine("\n");
 
-WageCalculation wage = new();
+//WageCalculation wage = new();
 
-wage.EmployeeWage("BridgeLabs", 20, 20, 100);
-wage.EmployeeWage("Amazon", 30, 20, 120);
+//wage.EmployeeWage("BridgeLabs", 20, 20, 100);
+//wage.EmployeeWage("Amazon", 30, 20, 120);
 
- 
+WageCalculation Amazon = new WageCalculation("Amazon", 20, 5, 100);
+Amazon.computewage();
+WageCalculation BridgeLabs = new WageCalculation("Bridge Labs", 25, 5, 120);
+BridgeLabs.computewage();
+
+
--- Employeewage/multicompanies.cs
+++ Employeewage/multicompanies.cs
@@ -11,9 +11,23 @@ namespace Employeewage
         public const int is_part_time = 1;
         public const int is_full_time = 2;
 
-        public int EmployeeWage(string company, int emprateperhr, int numofworkingdays, int maxhrpermnt)
+        public string company;
+        public int emprateperhr;
+        public int numofworkingdays;
+        public int maxhrpermnt;
+        public int totalempwage;
+
+        public WageCalculation(string company, int emprateperhr, int numofworkingdays, int maxhrpermnt)
         {
-            int emphr = 0, totalemphrs = 0, totalworkingdays = 0;
+            this.company = company;
+            this.emprateperhr = emprateperhr;
+            this.numofworkingdays = numofworkingdays;
+            this.maxhrpermnt = maxhrpermnt;
+                
+        }
+        public void computewage()
+        {
+            int emphr = 0, totalemphrs = 0, totalworkingdays = 0; 
 
             while (totalemphrs <= maxhrpermnt && totalworkingdays < numofworkingdays)
             {
@@ -34,15 +48,19 @@ namespace Employeewage
 
                 }
                 totalemphrs += emphr;
-                //Console.WriteLine("Total Working Days :" + totalworkingdays + "Employee hours : " + emphr);
-
+                Console.WriteLine("Working Day : " + totalworkingdays + " and Employee hours per day : " + emphr);
             }
             int totalempwage = totalemphrs * emprateperhr;
-            Console.WriteLine("Total Employee Wage for company : " + company + " is : " + totalempwage);
-            return totalempwage;
+            Console.WriteLine("\n");
+            Console.WriteLine("Total Working Days = " + totalworkingdays + " and Total Employee hours = " + totalemphrs);
+            Console.WriteLine("Total Employee Wage for company " + company + " is : " + totalempwage);
+            Console.WriteLine("\n");
         }
 
-
+        public string tostring()
+        {
+            return "Total Employee wage for company " + this.company + " is : " + this.totalempwage;
+        }
 
     }
 }
